a3b612f8fb255977201afd62f900a3cc255a88ca
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2022-11-23 12:11:26 -0500

    savepoint for 2014 data analysis and export

--- brfss_cdc_data/sas2csv.R
+++ brfss_cdc_data/sas2csv.R
@@ -1,21 +1,17 @@
-# options(stringsAsFactors=F, scipen = 999)
-# 
-# pkg = 'Hmisc'
-# if (!require(pkg, character.only = TRUE)) {
-#   install.packages(pkg)
-#   library(pkg, character.only = TRUE)
-# }
+options(stringsAsFactors=F, scipen = 999)
 
-library("devtools")
-install_version("Hmisc", version = "3.17-2")
+pkg = 'Hmisc'
+if (!require(pkg, character.only = TRUE)) {
+  install.packages(pkg)
+  library(pkg, character.only = TRUE)
+}
 
 
+setwd("C:/Users/cjong/Projects/python_projects/us_transportation_and_mental_health/brfss_cdc_data")
 
-brfss <- sasxport.get("CNTY07.xpt")
-write.csv(brfss, file = "CNTY07.csv")
 
-brfss <- sasxport.get("CNTY09.xpt")
-write.csv(brfss, file = "CNTY09.csv")
+brfss <- sasxport.get("MMSA2014.xpt")
+write.csv(brfss, file = "MMSA2014.csv")
 
 brfss <- sasxport.get("MMSA2019.xpt")
 write.csv(brfss, file = "MMSA2019.csv")
