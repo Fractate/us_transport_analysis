b748e9596c14eb411c80026d3cc10de6fe7b211c
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2023-05-13 11:59:18 -0400

    draft complete

--- geopanda_analysis/helper_classes/geoprocessor.py
+++ geopanda_analysis/helper_classes/geoprocessor.py
@@ -3,13 +3,10 @@ import pandas as pd
 import geopandas as gpd
 
 
-def aggregate_join_nyc_to_buffered_networkx_stations(new_column_suffix, nyc_gdf, networkx_df, meters):
+def aggregate_join_nyc_to_buffered_networkx_stations(new_column_suffix, nyc_gdf, _networkx_gdf, meters):
     
-    networkx_gdf = gpd.GeoDataFrame(
-        networkx_df,
-        geometry=gpd.points_from_xy(networkx_df.stop_lon, networkx_df.stop_lat)
-    )
-    networkx_gdf.geometry = networkx_gdf.buffer(meters/100000)
+    networkx_gdf = _networkx_gdf.copy()
+    networkx_gdf.geometry = networkx_gdf.buffer(meters)
 
     # Spatially join the two GeoDataFrames and aggregate the values of the 'column_to_summarize_1' and 'column_to_summarize_2' columns
     joined_mean_df = gpd.sjoin(nyc_gdf, networkx_gdf, how='left', op='intersects')\
